38352daffb12f4b78c79cf1212db5d6723b1a2f7
e.efimchik <e.efimchik@tinkoff.ru>
2023-09-26 16:55:52 +0300

    Initial Commit

---
+++ src/main/java/com/example/petproject239/PetProject239Application.java
@@ -0,0 +1,13 @@
+package com.example.petproject239;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetProject239Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetProject239Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/petproject239/PetProject239ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.petproject239;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetProject239ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
